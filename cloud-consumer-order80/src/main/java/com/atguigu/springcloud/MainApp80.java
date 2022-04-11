ad7d6760fdcaee622375fc443fdfecff22b39855
yuqiong <144647113@qq.com>
2022-04-11 23:04:31 +0800

    添加模块

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/MainApp80.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author A
+ * @date 2022/4/11
+ */
+@SpringBootApplication
+public class MainApp80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp80.class, args);
+    }
+}
---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -0,0 +1,43 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author A
+ * @date 2022/4/11
+ */
+@RestController
+public class OrderController {
+    public static final String PAYMENT_SRV_URL = "http://localhost:8001";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    /**
+     * 客户端用浏览器是get请求，但是底层实质发送post调用服务端8001
+     *
+     * @param payment 实体
+     * @return 结果
+     */
+    @GetMapping("/consumer/payment/create")
+    public CommonResult create(Payment payment) {
+        return restTemplate.postForObject(PAYMENT_SRV_URL + "/payment/create", payment, CommonResult.class);
+    }
+
+    /**
+     * 查询
+     *
+     * @param id id
+     * @return 结果
+     */
+    @GetMapping("/consumer/payment/get/{id}")
+    public CommonResult getPayment(@PathVariable Long id) {
+        return restTemplate.getForObject(PAYMENT_SRV_URL + "/payment/get/" + id, CommonResult.class, id);
+    }
+}
