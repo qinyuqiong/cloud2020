7a35fc9649cd67737b5631278b30204a25b36e1c
yuqiong <1446647113@qq.com>
2022-05-15 20:58:47 +0800

    docs(): 📝 添加注解

--- cloud-api-commons/src/main/java/com/atguigu/springcloud/entities/CommonResult.java
+++ cloud-api-commons/src/main/java/com/atguigu/springcloud/entities/CommonResult.java
@@ -5,17 +5,33 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
+ * 通用返回值
+ *
  * @author A
- * @date 2022/4/11
  */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CommonResult<T> {
+    /**
+     * 返回码
+     */
     private Integer code;
+    /**
+     * 返回消息
+     */
     private String message;
+    /**
+     * 返回体
+     */
     private T data;
 
+    /**
+     * 构造
+     *
+     * @param code    返回码
+     * @param message 返回值
+     */
     public CommonResult(Integer code, String message) {
         this(code, message, null);
     }
