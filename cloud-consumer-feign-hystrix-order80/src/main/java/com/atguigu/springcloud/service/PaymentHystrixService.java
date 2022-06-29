122277166044b9d13b596ce029e04053603e0daf
qinjiawei1 <qinjiawei@nbdeli.com>
2022-06-29 09:56:10 +0800

    feat(): ✨ Hystrix
    通过引入Hystrix使响应超时时返回指定值。但是每个接口都加会导致方法膨胀

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/service/PaymentHystrixService.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @author DWL
+ * @date 2022/6/29
+ */
+@Component
+@FeignClient(value = "CLOUD-PROVIDER-HYSTRIX-PAYMENT")
+public interface PaymentHystrixService {
+    @GetMapping("/payment/hystrix/ok/{id}")
+    String paymentInfoOK(@PathVariable("id") Integer id);
+
+    @GetMapping("/payment/hystrix/timeout/{id}")
+    String paymentInfoTimeOut(@PathVariable("id") Integer id);
+}
---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/PaymentHystrixMain8001.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author DWL
+ * @date 2022/6/29
+ */
+@SpringBootApplication
+@EnableEurekaClient //本服务启动后会自动注册进eureka服务中
+@EnableCircuitBreaker
+public class PaymentHystrixMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentHystrixMain8001.class, args);
+    }
+}
