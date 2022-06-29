413d0719d3ff62e012eb1492c6ead843c0356d71
qinjiawei1 <qinjiawei@nbdeli.com>
2022-06-29 17:23:47 +0800

    feat(): ✨ 添加cloud config

--- db2019.sql
+++ db2019.sql
@@ -21,16 +21,22 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- Table structure for payment
 -- ----------------------------
 DROP TABLE IF EXISTS `payment`;
-CREATE TABLE `payment`  (
-  `id` int NOT NULL,
-  `serial` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+CREATE TABLE `payment`
+(
+    `id`     int                                                           NOT NULL,
+    `serial` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci
+  ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of payment
 -- ----------------------------
-INSERT INTO `payment` VALUES (1, '2');
-INSERT INTO `payment` VALUES (31, '11');
+INSERT INTO `payment`
+VALUES (1, '2');
+INSERT INTO `payment`
+VALUES (31, '11');
 
 SET FOREIGN_KEY_CHECKS = 1;
