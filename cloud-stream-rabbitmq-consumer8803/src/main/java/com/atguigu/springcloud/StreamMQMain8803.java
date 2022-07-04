591cb123c18b9665261e13890b5865cd0c7b4389
qinjiawei1 <qinjiawei@nbdeli.com>
2022-07-04 15:53:30 +0800

    feat(): ✨ Stream 消息驱动

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/atguigu/springcloud/StreamMQMain8803.java
@@ -0,0 +1,15 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author DWL
+ * @date 2022/7/4
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class, args);
+    }
+}
