8c91787e1eccb335cbbdfcd1d692e4a9b270b75d
qinjiawei1 <qinjiawei@nbdeli.com>
2022-07-05 16:56:24 +0800

    feat(): ✨ sentinel 热点

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,6 +1,9 @@
 package com.atguigu.springcloud.alibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -26,8 +29,19 @@ public class FlowLimitController {
     }
 
     @GetMapping("/testD")
-    public String testD(){
-        int age = 10/0;
+    public String testD() {
+        int age = 10 / 0;
         return "---testD";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "dealHandler_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p2", required = false) String p2) {
+        return "------testHotKey";
+    }
+
+    public String dealHandler_testHotKey(String p1, String p2, BlockException exception) {
+        return "-----dealHandler_testHotKey";
     }
 }
